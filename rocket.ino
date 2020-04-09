c5d6e9f80422e29c34b71b7e5b7963bb87d289d7
nrhint <nathan@hintonclan.org>
2020-04-09 07:34:28 -0600

    Beginning with the sensor and learning how to track the data.

--- rocket.ino
+++ rocket.ino
@@ -1,9 +1,31 @@
+/*
+ * Nathan Hinton
+ * This is for the rocket that I am 3d printing. I am planning on putting the MPU9250 inside with a nano and using it to measure the stuff.
+ * I am strting with on the ground testing
+ */
+
+#include <MPU9250.h>
+
+MPU9250 IMU(Wire, 0x68);
+int status;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial) {}
 
+  // start communication with IMU 
+  status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  IMU.readSensor();//Read the data from the senor.
 }
